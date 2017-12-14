967488686ceac6cd6f056755173bf44d38171278
Pragmacmd <31473719+Pragmacmd@users.noreply.github.com>
2017-12-14 11:47:30 +0100

    enleve les "print" inutiles

--- index.py
+++ index.py
@@ -17,15 +17,11 @@ while NOTIFICATION_ACTIVE is False:
 
 #boucle infini tant que la notification est activée
 while NOTIFICATION_ACTIVE is True:
-    print(datetime.now())
     RESEAUX_SCANNES = [] #stock les adresses Mac scannees
     try:
         RESEAUX_SCANNES = fonction_scanner(4.0) #scan de X secondes des BLE du périmètre
     except:
-        print("error relance scan")
         RESEAUX_SCANNES = fonction_scanner(4.0) #scan de X secondes des BLE du périmètre
-    print("scan BLE ok")
-    print(RESEAUX_SCANNES)
 
     RESEAUX_TRAITEMENT = [] #stockage provisoire des adresses Mac à traiter
     RESEAUX_TRAITEMENT = RESEAUX_SCANNES #liberation de la liste de BLE scannes
@@ -34,19 +30,10 @@ while NOTIFICATION_ACTIVE is True:
         CAPTEURS_SCANNES = []
         #vérification liste de capteur scannes
         CAPTEURS_SCANNES = test_adresse_mac(RESEAUX_TRAITEMENT, LISTE_ADRESSE_MAC)
-        print("adresse mac reconnues ok")
-        print(CAPTEURS_SCANNES)
 
         if CAPTEURS_SCANNES != []: #traitement délai notification si data du traitement adresse mac
             CAPTEURS_NOTIFIES = []
-            #vérification latence notification > 60 secondes
-            CAPTEURS_NOTIFIES = comparaison_heure(CAPTEURS_SCANNES)
-            print("délai notification controle")
-            print(CAPTEURS_NOTIFIES)
+            CAPTEURS_NOTIFIES = comparaison_heure(CAPTEURS_SCANNES) #vérification latence notification > 60 secondes
 
             if CAPTEURS_NOTIFIES != []: #envoi d'une notification si data du traitement délai notification
-                #envoi notification
                 boucle_notification(CAPTEURS_NOTIFIES)
-                print("emails envoyé")
-
-print(CAPTEURS_NOTIFIES)
